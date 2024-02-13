14dcbd1c02996f0ef84e2e66b4cdabafffb1f8a0
Anthony Maldonado <shaulinpriest10@gmail.com>
2024-02-13 15:34:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("testing and playing around with terminal git commands");
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = 0;
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+const letters = document.querySelectorAll(`.letter`);
+const intervalId = setInterval(function () {
+    for (let letter of letters) {
+        letter.style.color = randomRGB();
+    }
+}, 500);
